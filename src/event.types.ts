e09c128d21c36f4b74c5209cbcc890bb80ac22e7
AhadCove <ahadcove@gmail.com>
2021-11-26 21:19:48 -0500

    first push

---
+++ src/event.types.ts
@@ -0,0 +1,196 @@
+// Used for external things like our SDK websocket and rest apis
+
+import { LumiaActivityCommandTypes } from './activity.types';
+
+// Packets that will be sent through socket and rest apis
+export interface ILumiaSendPack {
+	type: LumiaActivityCommandTypes;
+	params: {
+		value: string | { r: number; g: number; b: number };
+		lights?: Array<ILumiaLight>;
+		hold?: boolean; // Sets this command to default or not
+		skipQueue?: boolean; // Skips the queue and instantly turns to this color
+
+		platform?: LumiaPlatforms;
+
+		// Used for TTS
+		voice?: string;
+		volume?: number;
+
+		// Mainly used for RGB color and Hex color types
+		brightness?: number;
+		transition?: number;
+		duration?: number;
+
+		extraSettings?: { username?: string; bits?: number }; // Mainly used to pass in variables for things like TTS or Chat bot
+	};
+}
+
+export interface ILumiaEvent {
+	origin: EventOrigins;
+	subOrigin?: EventOrigins;
+	type: LumiaEventTypes;
+	data: ILumiaEventStateBody | ILumiaEventChatCommandBody | ILumiaEventChatBody | ILumiaEventAlertBody | ILumiaEventStateBody;
+}
+
+export interface ILumiaEventChatCommandBody {
+	username?: string;
+	command?: string;
+}
+
+export interface ILumiaEventChatBody {
+	type: string;
+	data: {
+		id: string;
+		username: string;
+		userId: string;
+		userColor: string;
+		userColorRgb: string;
+		avatar: string;
+		platform: string;
+		badgesRaw: string;
+		hasEmotes: boolean;
+		emotes: string;
+		rawMessageWithoutEmotes: string;
+		emotesRaw: string;
+		channel: string;
+		message: string;
+		user: {
+			'badge-info': Array<unknown>;
+			badges: Array<unknown>;
+			'client-nonce': string;
+			color: string;
+			'display-name': string;
+			emotes: string;
+			'first-msg': boolean;
+			flags: string;
+			id: string;
+			mod: boolean;
+			'room-id': string;
+			subscriber: boolean;
+			'tmi-sent-ts': string;
+			turbo: boolean;
+			'user-id': string;
+			'user-type': string;
+			'emotes-raw': string;
+			'badge-info-raw': string;
+			'badges-raw': string;
+			username: string;
+			'message-type': string;
+			isSelf: boolean;
+			vip: boolean;
+			tier3: boolean;
+			tier2: boolean;
+			tier1: boolean;
+			follower: boolean;
+		};
+	};
+	raw: {
+		channel: string;
+		message: string;
+		user: {
+			'badge-info': Array<unknown>;
+			badges: Array<unknown>;
+			'client-nonce': string;
+			color: string;
+			'display-name': string;
+			emotes: string;
+			'first-msg': boolean;
+			flags: string;
+			id: string;
+			mod: boolean;
+			'room-id': string;
+			subscriber: boolean;
+			'tmi-sent-ts': string;
+			turbo: boolean;
+			'user-id': string;
+			'user-type': string;
+			'emotes-raw': string;
+			'badge-info-raw': string;
+			'badges-raw': string;
+			username: string;
+			'message-type': string;
+			isSelf: boolean;
+			vip: boolean;
+			tier3: boolean;
+			tier2: boolean;
+			tier1: boolean;
+			follower: boolean;
+		};
+	};
+}
+
+export interface ILumiaEventAlertBody {
+	// Streamlabs origin
+	type?: 'follow';
+	message?: unknown;
+	for?: 'twitch_account';
+	event_id?: string;
+}
+
+export interface ILumiaEventStateBody {
+	on: number;
+	fuze: number;
+	streamMode: number;
+}
+
+export interface ILumiaLight {
+	type: LightBrands;
+	id: string | number;
+}
+
+export type LumiaPlatforms = 'twitch' | 'youtube' | 'facebook' | 'trovo' | 'glimesh';
+type LightBrands =
+	| 'hue'
+	| 'nanoleaf'
+	| 'nanoleaf2'
+	| 'lifx'
+	| 'tplink'
+	| 'yeelight'
+	| 'cololight'
+	| 'tuya'
+	| 'smartlife'
+	| 'wyze'
+	| 'wiz'
+	| 'homeassistant'
+	| 'govee'
+	| 'wled'
+	| 'magichome'
+	| 'logitech'
+	| 'razer'
+	| 'corsair'
+	| 'steelseries'
+	| 'overlay'
+	| 'elgato';
+
+export enum LumiaEventTypes {
+	STATES = 'states',
+	CHAT = 'chat',
+	COMMAND = 'command',
+	TWITCH_POINTS = 'twitch_point',
+	TWITCH_EXTENSIONS = 'twitch_extension',
+	TROVO_SPELL = 'twitch_extension',
+	PULSE = 'pulse',
+	ALERTS = 'alert',
+}
+
+type EventOrigins =
+	| 'lumia'
+	| 'twitch'
+	| 'youtube'
+	| 'facebook'
+	| 'glimesh'
+	| 'trovo'
+	| 'streamlabs'
+	| 'streamelements'
+	| 'extralife'
+	| 'donordrive'
+	| 'tiltify'
+	| 'patreon'
+	| 'tipeeestream'
+	| 'treatstream'
+	| 'discord'
+	| 'obs'
+	| 'slobs'
+	| 'pulsoid'
+	| 'paypal';
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+export {
+	LumiaActivityCommandTypes,
+	LumiaExternalActivityCommandTypes,
+	LumiaAlertValues,
+	LumiaActivityOriginTypes,
+	ActivityApiValueType,
+	LumiaActivityNoValueTypes,
+	LumiaActivityTestType,
+} from './activity.types';
+export { ILumiaSendPack, ILumiaEvent, ILumiaEventChatCommandBody, ILumiaEventChatBody, ILumiaEventAlertBody, ILumiaEventStateBody, ILumiaLight, LumiaPlatforms, LumiaEventTypes } from './event.types';
