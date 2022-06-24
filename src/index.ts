901a9c99ccebfd6493054d6730d838c67d9ab914
ahadcove <me@ahadcove.com>
2022-06-23 21:30:44 -0400

    fix: export of LumiaAlertFriendlyValues

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 export {
-	LumiaActivityCommandTypes,
-	LumiaExternalActivityCommandTypes,
-	LumiaAlertValues,
-	LumiaActivityOriginTypes,
-	LumiaActivityApiValueType,
-	LumiaActivityNoValueTypes,
-	LumiaActivityTestType,
+    LumiaActivityCommandTypes,
+    LumiaExternalActivityCommandTypes,
+    LumiaAlertValues,
+    LumiaAlertFriendlyValues,
+    LumiaActivityOriginTypes,
+    LumiaActivityApiValueType,
+    LumiaActivityNoValueTypes,
+    LumiaActivityTestType,
 } from './activity.types';
 export { ILumiaSendPack, ILumiaEvent, ILumiaEventChatCommandBody, ILumiaEventChatBody, ILumiaEventAlertBody, ILumiaEventStateBody, ILumiaLight, LumiaPlatforms, LumiaEventTypes } from './event.types';
